5cf34c12f7f6a5ed431da95268f9b7b2bea32e8e
hsiaofung.chou <hsiaofung.chou@gmail.com>
2017-07-13 14:16:55 +0800

    第一版，給Amos看過，PC版OK，RWD還有一些要切

---
+++ sass/js/light_box.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    	$('#btn-light-box').click(function(){    		
+    			$('.lightbox').show();               
+    		}   		
+    	});    	
+    	$('.btn-close').click(function(){
+    		$('.lightbox').hide();
+    	});
+});
+
+
